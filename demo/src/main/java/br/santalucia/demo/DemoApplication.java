ea8eaa6b72f229efbcb82de6aa8dbf4ca3825e6a
Maromo <professormoraes@gmail.com>
2022-10-20 22:34:27 -0300

    Aula do dia 20 de outubro

---
+++ demo/src/main/java/br/santalucia/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package br.santalucia.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
